6335f60b77cac73f562531ef825037809c21cd8d
MichaelTr7 <MichaelTr7Stackoverflow@gmail.com>
2022-05-05 12:21:46 -0400

    Quality of life improvements.

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,18 +1,15 @@
 
 window.onload = function(){
   console.log("Test");
-  let Links = document.getElementsByClassName('Links');
-  for(Index=0; Index < Links.length; Index++){
-    Links[Index].addEventListener("ontouchstart",Clicked);
-    
+  let Links = document.getElementsByClassName('Containers');
+  for(Index=3; Index < Links.length; Index++){
+    Links[Index].addEventListener("click",Clicked);
   }
-  
-  
-  
-  
 }
 
 function Clicked(){
-  
-  console.log("Clicked");
+    console.log("Clicked");
+    this.classList.remove('Button_Pressed');
+    void this.offsetWidth
+    this.classList.add('Button_Pressed');
 }
