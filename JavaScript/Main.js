7d98bf0fee25de747234e870b0169f668c0ec854
MichaelTr7 <MichaelTr7Stackoverflow@gmail.com>
2022-05-06 13:55:19 -0400

    Changed links to buttons.
    Disables 3D touch previews.

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,17 +1,23 @@
 
 window.onload = function(){
   console.log("Test");
-  let Links = document.getElementsByClassName('Containers');
-  for(Index=3; Index < Links.length; Index++){
+  let Links = document.getElementsByClassName('Links');
+  for(Index=0; Index < Links.length; Index++){
     Links[Index].addEventListener("click",Clicked);
   }
   
 }
 
 function Clicked(){
-    console.log("Clicked");
-    this.classList.remove('Button_Pressed');
-    void this.offsetWidth;
-    this.classList.add('Button_Pressed');
+    Links = {
+      "Instagram":"https://www.instagram.com/drawingmycompass/",
+      "DeviantArt":"https://www.deviantart.com/drawingmycompass",
+      "Community Spotify Playlist":"https://open.spotify.com/playlist/1sHTvHLJuF3mtRILaiSmki?si=6abbb4a832f34eb2"
+    };
+  
+    Target_Link = Links[String(this.innerHTML)];
+    window.location.href = Target_Link;
+
+
 }
 
