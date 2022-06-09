62895a83befa22d74bf549192f1832f5dc80f304
MichaelTr7 <MichaelTr7Stackoverflow@gmail.com>
2022-06-09 10:50:56 -0400

    Control Centre Card Updated

--- Pages/Code_JavaScript/Display_Cards.js
+++ Pages/Code_JavaScript/Display_Cards.js
@@ -4,7 +4,7 @@ function Display_Card(){
   let About_Card = document.getElementsByClassName('About_Card')[0];
   let Network_Card = document.getElementsByClassName('Network_Card')[0];
   let Battery_Card = document.getElementsByClassName('Battery_Card')[0];
-  let Notification_Center_Card = document.getElementsByClassName('Notification_Center_Card')[0];
+  let Control_Center_Card = document.getElementsByClassName('Control_Center_Card')[0];
   let Date_Card = document.getElementsByClassName('Date_Card')[0];
   
   if(Button_Identifier == "About_Button"){
@@ -12,7 +12,7 @@ function Display_Card(){
     // About_Card.classList.remove("Show_About_Card");
     Network_Card.classList.remove("Show_Network_Card");
     Battery_Card.classList.remove("Show_Battery_Card");
-    Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+    Control_Center_Card.classList.remove("Show_Control_Center_Card");
     Date_Card.classList.remove("Show_Date_Card");
     About_Card.classList.toggle("Show_About_Card");
   }
@@ -21,7 +21,7 @@ function Display_Card(){
     About_Card.classList.remove("Show_About_Card");
     // Network_Card.classList.remove("Show_Network_Card");
     Battery_Card.classList.remove("Show_Battery_Card");
-    Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+    Control_Center_Card.classList.remove("Show_Control_Center_Card");
     Date_Card.classList.remove("Show_Date_Card");
     Network_Card.classList.toggle("Show_Network_Card");
   }
@@ -30,7 +30,7 @@ function Display_Card(){
     About_Card.classList.remove("Show_About_Card");
     Network_Card.classList.remove("Show_Network_Card");
     // Battery_Card.classList.remove("Show_Battery_Card");
-    Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+    Control_Center_Card.classList.remove("Show_Control_Center_Card");
     Date_Card.classList.remove("Show_Date_Card");
     Battery_Card.classList.toggle("Show_Battery_Card");
   }
@@ -39,16 +39,16 @@ function Display_Card(){
     About_Card.classList.remove("Show_About_Card");
     Network_Card.classList.remove("Show_Network_Card");
     Battery_Card.classList.remove("Show_Battery_Card");
-    // Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+    // Control_Center_Card.classList.remove("Show_Control_Center_Card");
     Date_Card.classList.remove("Show_Date_Card");
-    Notification_Center_Card.classList.toggle("Show_Notification_Center_Card");
+    Control_Center_Card.classList.toggle("Show_Control_Center_Card");
   }
   else if(Button_Identifier == "Date_Button"){
     console.log("Date button pressed");
     About_Card.classList.remove("Show_About_Card");
     Network_Card.classList.remove("Show_Network_Card");
     Battery_Card.classList.remove("Show_Battery_Card");
-    Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+    Control_Center_Card.classList.remove("Show_Control_Center_Card");
     // Date_Card.classList.remove("Show_Date_Card");
     Date_Card.classList.toggle("Show_Date_Card");
   }    
@@ -58,12 +58,12 @@ function Unfocus_Cards(){
   let About_Card = document.getElementsByClassName('About_Card')[0];
   let Network_Card = document.getElementsByClassName('Network_Card')[0];
   let Battery_Card = document.getElementsByClassName('Battery_Card')[0];
-  let Notification_Center_Card = document.getElementsByClassName('Notification_Center_Card')[0];
+  let Control_Center_Card = document.getElementsByClassName('Control_Center_Card')[0];
   let Date_Card = document.getElementsByClassName('Date_Card')[0];
   About_Card.classList.remove("Show_About_Card");
   Network_Card.classList.remove("Show_Network_Card");
   Battery_Card.classList.remove("Show_Battery_Card");
-  Notification_Center_Card.classList.remove("Show_Notification_Center_Card");
+  Control_Center_Card.classList.remove("Show_Control_Center_Card");
   Date_Card.classList.remove("Show_Date_Card");
 }
 
