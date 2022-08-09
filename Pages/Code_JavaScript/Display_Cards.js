c9bdd18dd157dac1bf893e480bb4ad650b61beb6
MichaelTr7 <67344799+MichaelTr7@users.noreply.github.com>
2022-08-09 15:11:23 -0400

    Activity Rings Animation

--- Pages/Code_JavaScript/Display_Cards.js
+++ Pages/Code_JavaScript/Display_Cards.js
@@ -9,6 +9,37 @@ function Display_Card(){
   let Widgets_Card_Background = document.getElementsByClassName('Widgets_Glass_Background')[0];
   
   
+  if(Button_Identifier == "Date_Button"){
+    console.log("Show Widgets");
+    var Movements = document.getElementsByClassName("Activity_Movements");
+    var Blue_Movement = Movements[2];
+    Blue_Movement.classList.add("Rotate_Blue_Movements");
+    var Green_Movement = Movements[1];
+    Green_Movement.classList.add("Rotate_Green_Movements");
+    var Red_Movement = Movements[0];
+    Red_Movement.classList.add("Rotate_Red_Movements");
+
+    var Blue_Ring = document.getElementById("Blue_Ring");
+    Blue_Ring.classList.add("Blue_Ring_Animation");
+    var Green_Ring = document.getElementById("Green_Ring");
+    Green_Ring.classList.add("Green_Ring_Animation");
+    var Red_Ring = document.getElementById("Red_Ring");
+    Red_Ring.classList.add("Red_Ring_Animation");
+    
+    let Start_Nubs = document.getElementsByClassName("Start_Activity_Circles");
+    Start_Nubs[0].classList.add("Show_Red_Nub");
+    Start_Nubs[1].classList.add("Show_Green_Nub");
+    Start_Nubs[2].classList.add("Show_Blue_Nub");
+
+    
+    document.getElementById("Move_Label").classList.add("Show_Move_Label");
+    document.getElementById("Exercise_Label").classList.add("Show_Exercise_Label");
+    document.getElementById("Stand_Label").classList.add("Show_Stand_Label");
+
+    
+    
+  }
+  
   if(Button_Identifier == "About_Button"){
     // About_Card.classList.remove("Show_About_Card");
     Network_Card.classList.remove("Show_Network_Card");
