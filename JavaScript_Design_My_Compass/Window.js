ca09e50925fe0d18a1d5c15ed01e910031450401
MichaelTr7 <67344799+MichaelTr7@users.noreply.github.com>
2022-12-03 15:24:19 -0500

    Update Window.js

--- JavaScript_Design_My_Compass/Window.js
+++ JavaScript_Design_My_Compass/Window.js
@@ -9,5 +9,10 @@ window.onload = function(){
 function Touchscreen_Setup(){
   if(window.matchMedia("(pointer: coarse)").matches) {
     document.getElementById("Hover_Highlight").style.backgroundColor = "transparent";
+    let Gig_Tiles = document.getElementsByClassName('Gig_Tiles');
+    for(Index=0; Index < Gig_Tiles.length; Index++){
+      Gig_Tiles[Index].style.height = "90vw";
+      Gig_Tiles[Index].style.width = "90vw";
+    }
 }  
 }
